626ac83ba80f45779e9b63dc2ed1f059cf582859
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2024-12-05 13:53:27 +0100

    Update pdf_processor.py

--- app/services/pdf_processor.py
+++ app/services/pdf_processor.py
@@ -10,6 +10,7 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_openai import ChatOpenAI
 
+####pdf processor
 class PDFProcessor:
     def __init__(self):
         self.vectorstore = None
